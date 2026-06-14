164f923f20790e3233a0f98f4f731a12259db579
ShiYiShang-Bin <562240133@qq.com>
2026-06-14 15:39:49 +0800

    11

--- main.cpp
+++ main.cpp
@@ -2,23 +2,16 @@
 
 
 int main() {
-    initgraph(WINDOW_WID,WINDOW_HEI); 
+    initgraph(WINDOW_WID,WINDOW_HEI);
 	setbkcolor(BROWN);
 	cleardevice();
 	//设置文字透明背景
 	setbkmode(TRANSPARENT);
-	InitMenu();  
-	ExMessage msg;
+	InitMenu();
 
 	int result = 1;
 	while (result) {
-		//game() 
-		if (peekmessage(&msg, EX_KEY)) {
-			if (msg.message == WM_KEYDOWN &&
-				msg.vkcode == VK_ESCAPE) {
-				result = menu();
-			}
-		}
+		result = menu();
 	}
     closegraph();
     return 0;
