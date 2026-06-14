efcf623e04e316e55a58a58db1408a0379431dfe
chumengD <mildredcain9@gmail.com>
2026-06-14 15:37:38 +0800

    修改menu

--- main.h
+++ main.h
@@ -3,6 +3,11 @@
 #include <iostream>
 #include <conio.h>
 #include <cstdlib>
+#include <tuple>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+#include <sstream>
 
 #define WINDOW_WID 650
 #define WINDOW_HEI 500
